f80ef43f0820fecfd7b5b450f33f05413d2a57c4
Ilya Kuzmin <68997547+kuzmin-i@users.noreply.github.com>
2022-03-01 10:41:21 +0300

    updated news

--- Components/Media/news/data.js
+++ Components/Media/news/data.js
@@ -15,81 +15,114 @@ const interviewData = {
 const newsList = [
   {
     title: {
-      ru: "ТПО «Резерв» обновило сайт ко дню рождения дизайнера и архитектора Даниила Колодия",
+      ru: "Выставочный проект ТПО «Резерв» представлен в разделе «Архкаталог» на выставке АРХ Москва 2020",
       en: (
         <>
-          TPO Reserv updated the website for the birthday of designer and
-          architect Daniil Kolodiy
+          TPO Reserve's exhibition project is presented in the ArchCatalog
+          section at the ARCH Moscow 2020 exhibition
         </>
       ),
     },
     date: {
-      ru: <>9 декабря 2021</>,
-      en: <>9 December 2021</>,
+      ru: <>8 октября 2020</>,
+      en: <>8 October 2021</>,
     },
   },
   {
     title: {
       ru: (
         <>
-          Владимир Плоткин наградил Марину Ксенофонтову медалью за переработки
+          Специалисты ТПО «РЕЗЕРВ» получат статус международных экспертов в
+          сфере BIM
         </>
       ),
       en: (
-        <>Vladimir Plotkin awarded Marina Ksenofontova a medal for recycling</>
+        <>TPO RESERVE specialists to receive international BIM expert status</>
       ),
     },
     date: {
-      ru: <>1 декабря 2021</>,
-      en: <>1 December 2021</>,
+      ru: <>22 марта 2019</>,
+      en: <>22 March 2019</>,
     },
   },
   {
     title: {
       ru: (
-        <>Дарья Веркеенко взяла интервью у одногруппников Владимира Плоткина</>
+        <>
+          «ТПО РЕЗЕРВ» заняло первое место в престижном конкурсе «BIM-технологии
+          2016»
+        </>
       ),
-      en: <>Daria Verkeenko interviews Vladimir Plotkin's classmates</>,
+      en: (
+        <>
+          "TPO RESERVE wins first place in the prestigious BIM Technology 2016
+          competition
+        </>
+      ),
     },
     date: {
-      ru: <>26 ноября 2021</>,
-      en: <>26 November 2021</>,
+      ru: <>16 июня 2018</>,
+      en: <>16 June 2018</>,
     },
   },
   {
     title: {
       ru: (
-        <>Дарья Веркеенко взяла интервью у одногруппников Владимира Плоткина</>
+        <>
+          В рамках проекта "Архитектурный год 2017" состоялся public talk с
+          Владимиром Плоткиным
+        </>
       ),
-      en: <>Daria Verkeenko interviews Vladimir Plotkin's classmates</>,
+      en: (
+        <>
+          Public talk with Vladimir Plotkin took place as part of the project
+          "Architectural Year 2017
+        </>
+      ),
     },
     date: {
-      ru: <>26 ноября 2021</>,
-      en: <>26 November 2021</>,
+      ru: <>15 июня 2018</>,
+      en: <>15 June 2018</>,
     },
   },
   {
     title: {
       ru: (
-        <>Дарья Веркеенко взяла интервью у одногруппников Владимира Плоткина</>
+        <>
+          Российское архитектурное бюро «ТПО «РЕЗЕРВ» и «СЕТЕК ИНЖИНИРИНГ»
+          договорились о создании консорциума.
+        </>
       ),
-      en: <>Daria Verkeenko interviews Vladimir Plotkin's classmates</>,
+      en: (
+        <>
+          The Russian architectural bureau TPO RESERVE and SETEK ENGINEERING
+          have agreed to form a consortium.
+        </>
+      ),
     },
     date: {
-      ru: <>26 ноября 2021</>,
-      en: <>26 November 2021</>,
+      ru: <>15 мая 2018</>,
+      en: <>15 May 2018</>,
     },
   },
   {
     title: {
       ru: (
-        <>Дарья Веркеенко взяла интервью у одногруппников Владимира Плоткина</>
+        <>
+          Началась подготовка к строительству крупнейшего медицинского центра в
+          Коммунарке
+        </>
       ),
-      en: <>Daria Verkeenko interviews Vladimir Plotkin's classmates</>,
+      en: (
+        <>
+          Preparations have begun for the construction of the largest medical
+          centre in Kommunarka
+        </>
+      ),
     },
     date: {
-      ru: <>26 ноября 2021</>,
-      en: <>26 November 2021</>,
+      ru: <>24 апреля 2018</>,
+      en: <>24 April 2018</>,
     },
   },
 ];
